fe85e5af596531a1a9d6f45ec1c92217248c320a
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-09 18:42:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,11 @@ def prime?(num)
   if num < 0 
     false 
   elsif num > 1 
-    
+    test_numbers = (2..num)
+    test_numbers.each do |test|
+      return false if num % test == 0 
+    end
+    true 
   end 
   if num > 2 
     test_numbers = (2...num)
