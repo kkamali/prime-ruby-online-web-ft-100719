9e505e74638de135e5ddf6f7cadfdfaf2160e284
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-09 18:32:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 def prime?(num)
   if num > 1 
     test_numbers = (2...Math.sqrt(num))
-    test_numbers.each
+    test_numbers.each do |test|
+      test / num 
+    end
   else
     false 
   end
